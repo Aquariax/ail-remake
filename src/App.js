e2898420e2e4af74537f8fc0239b6384ab270ec7
aquariax <roberthrrs8@gmail.com>
2020-12-20 04:09:49 -0500

    created designs for header, footer and navbar components and gave footer links

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Navbar from './components/layout/Navbar';
 import Header from './components/layout/Header';
+import Footer from './components/layout/Footer';
 import './components/layout/styles/compiled/main.css';
 function App() {
 	return (
@@ -8,6 +9,7 @@ function App() {
 			<Navbar />
 			<Header />
 			<h4>Hello World</h4>
+			<Footer />
 		</div>
 	);
 }
--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,23 +1,25 @@
 import React from 'react';
-import logo from '../images/edp-logo.svg';
+import logo from '../images/edp-logo-2.svg';
 
 const Navbar = () => {
 	return (
 		<div className='navigation'>
 			<div className='logo'>
-				<img src={logo} alt='edp-logo' />
+				<img src={logo} alt='edp-logo' className='logo-main' />
 			</div>
 			<div className='nav'>
 				<ul className='nav_menu'>
-					<li className='nav_menu_item'>
-						<a href='#!'>Language</a>
+					<li className='nav_menu_item button'>
+						<p>
+							Language <i className='fas fa-angle-down lang'></i>
+						</p>
 					</li>
 				</ul>
 				<ul className='nav_menu second'>
-					<li className='nav_menu_item'>
+					<li className='nav_menu_item sign-up'>
 						<a href='#!'>Sign Up</a>
 					</li>
-					<li className='nav_menu_item'>
+					<li className='nav_menu_item login'>
 						<a href='#!'>Log In</a>
 					</li>
 				</ul>
