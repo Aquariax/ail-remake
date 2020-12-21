19ddc65f57d0a5b89895ef3ea7f7aba01ec306e2
aquariax <roberthrrs8@gmail.com>
2020-12-20 20:06:41 -0500

    created access component styled navigation and access and started cards component for cards carousel

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Header from './components/layout/Header';
 import Footer from './components/layout/Footer';
 import Top from './components/layout/Top';
 import Bottom from './components/layout/Bottom';
+import Access from './components/layout/Access';
 import './components/layout/styles/compiled/main.css';
 function App() {
 	return (
 		<div className='App'>
 			<Navbar />
 			<Header />
+			<Access />
 			<Top />
 			<h4>Hello World</h4>
 			<Bottom />
---
+++ src/components/layout/Access.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Access = () => {
+	return (
+		<div className='code'>
+			<input
+				type='text'
+				placeholder='Class Access Code'
+				name='code'
+				className='code_input'
+			/>
+		</div>
+	);
+};
+
+export default Access;
